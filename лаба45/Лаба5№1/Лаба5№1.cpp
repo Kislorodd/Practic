6a426b9706b9111af9b3b5325fdd16d1ae4c733b
Kislorodd <antonromaskov2003@gmail.com>
2021-04-23 12:39:46 +0300

    кек

---
+++ лаба45/Лаба5№1/Лаба5№1.cpp
@@ -0,0 +1,37 @@
+﻿// Дано приложение, в котром нет символа -. Определите количество букв o в первом слове(учтите, что пробелы могут быть до слова)
+
+#include <iostream>
+#include <string>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	int ind = 0;
+	string str;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите строку" << endl;
+	getline(cin, str);
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] != ' ')
+		{
+			ind = i;
+			break;
+		}
+	}
+	for (int i = ind; i < str.length(); i++)
+	{
+		if (str[i] == 'o')
+		{
+			n++;
+		}
+		else if (str[i] == ' ')
+		{
+			break;
+		}
+	}
+	cout << n << endl;
+}
